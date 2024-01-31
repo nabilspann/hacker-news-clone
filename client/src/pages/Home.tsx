3b5d507e60ae8da23d1551646977cb3b71e3aa94
Nabil Spann <nabilspann@gmail.com>
2024-01-30 20:07:11 -0700

    Using onmount method

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,16 @@
-import { For, createResource } from "solid-js";
+import { For, createResource, createSignal, onMount } from "solid-js";
 import { getAllPosts, getTest } from "../apiCalls/CommentSectionCalls";
 import PostHeadline from "../components/PostHeadline";
 import { A } from "@solidjs/router";
 
 const Home = () => {
-  const [posts] = createResource(getAllPosts);
-  const [test] = createResource(getTest);
+  // const [posts] = createResource(getAllPosts);
+  const [test, setTest] = createSignal("");
+
+  onMount(async () => {
+    const testRes = await getTest();
+    setTest(testRes);
+  });
 
   return (
     <div class="flex flex-col justify-center items-center">
@@ -13,7 +18,7 @@ const Home = () => {
       <A href="/create-post" class="p-3 m-2 bg-sky-500 rounded-lg">
         + Create a new post
       </A>
-      <For each={posts()}>
+      {/* <For each={posts()}>
         {(post) => (
           <PostHeadline
             title={post.title}
@@ -21,7 +26,7 @@ const Home = () => {
             username={post.username}
           />
         )}
-      </For>
+      </For> */}
     </div>
   );
 };
