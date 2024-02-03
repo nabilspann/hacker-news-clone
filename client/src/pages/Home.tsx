7d8f90bcd3ca9e44e823cc5d6014e2f56f616c02
Nabil Spann <nabilspann@gmail.com>
2024-02-02 21:39:20 -0700

    Adding console logging DEV

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { For, createResource, createSignal, onMount } from "solid-js";
+import { DEV, For, createResource, createSignal, onMount } from "solid-js";
 import { getAllPosts } from "../apiCalls/CommentSectionCalls";
 import PostHeadline from "../components/PostHeadline";
 import { A } from "@solidjs/router";
@@ -6,6 +6,7 @@ import { A } from "@solidjs/router";
 const Home = () => {
   const [posts] = createResource(getAllPosts);
 
+  console.log("DEV", DEV);
   return (
     <div class="flex flex-col justify-center items-center">
       <A href="/create-post" class="p-3 m-2 bg-sky-500 rounded-lg">
