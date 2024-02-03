3579eace9c1c485a999bfa150a5008c4856fe393
Nabil Spann <nabilspann@gmail.com>
2024-02-02 22:40:04 -0700

    Fix domain name

--- client/src/utils/createAuthStore.ts
+++ client/src/utils/createAuthStore.ts
@@ -17,7 +17,7 @@ const authStore = () => {
   const signOut = async () => {
     try{
       await deleteCookie(
-        DEV ? "localhost" : "nabil-hacker-news-clone.netlify.app"
+        DEV ? "localhost" : "nabil-hacker-news-clone.netlify"
       );
       setIsAuthorized(false);
     }catch(err){
